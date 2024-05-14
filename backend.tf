a1da5343e168743ea85aeff4bd2d35c93a2e6de1
Helio Soares <helio_soares@outlook.com>
2024-05-14 17:43:51 -0300

    test

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "terraform-bucket-4soat-s3"
+    bucket  = "terraform-4soat-s3"
     key     = "terraform/state/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
